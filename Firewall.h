ef935468f35a4f60f43a307e677b5edcab8f7e33
OfirW3 <ofir.d.weit@gmail.com>
2025-08-31 16:11:28 +0300

    Saving changes

--- Firewall.h
+++ Firewall.h
@@ -24,38 +24,37 @@ typedef uint16 pincode;
 typedef uint8 subkey;//?
 typedef uint32 rounds; //?
 
-enum e_action{
+typedef enum e_action{
     permit = 1,
     drop = 2
 }action;
 
-struct s_network
+typedef struct s_network
 {
     ip_address ip : 32; // 32 bits for IP address
     CIDR subnet : 5; // 5 bits for CIDR (0-32)
 }network;
 
-struct s_user{
+typedef struct s_user{
     uint8 username[16]; // Username up to 31 characters + null terminator
     pincode pin : 14; // 10 bits for pincode (0-10000)
     bool root : 1; // 1 bit for root status
 }user;
 
-struct s_stdcl{ // standard control list structure
-    uint8 identifier[16]; // Identifier up to 15 characters + null terminator
+typedef struct s_stdce{ // standard control entry structure
     action act : 3 // 3 bits for action
     network net; // network structure
 
-}stdacl;
+}stdace;
 
+typedef stdace[100] acl; // access control list with max 100 entries
 
-
-struct s_rootkey{
+typedef struct s_rootkey{
     subkey key : 10; // 10 bits for rootkey
     rounds n : 23; // 23 bits for rounds (0-8000000)
 }rootkey;
 
-struct s_interface
+typedef struct s_interface
 {
     inter_num id : 3; //3 bits - 8 interfaces
     mac_address mac : 48; //48 bits for mac_address
@@ -68,4 +67,5 @@ struct s_interface
     level sec_level : 4; //4 bits for security level (0-16)
     user account; //user structure
     rootkey key; //rootkey structure ?
-};
+    acl access_list; //access control list
+}interface;
