c39c001c2b4e20de0f7b665f03272883f9c8ff90
אופיר וייט <ofir.d.weit@gmail.com>
2025-09-02 11:35:31 +0300

    Progress

--- Firewall.c
+++ Firewall.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "Firewall.h"
+
+dynamic *dynMake_(unsigned int entrysize){
+    dynamic *p;
+    unsigned int size = sizeof(struct s_dynamic) + (BLOCK_SIZE * entrysize); //Size of the minimal dynamic array length + size of the entries
+    p = (dynamic*)(malloc(size));
+    assert(p);
+    zero(p, size);
+
+    p->count = 0;
+    p->capacity = BLOCK_SIZE;
+    p->size = BLOCK_SIZE * entrysize; 
+
+    return p;
+}
 
 int main(){
-    printf("%d",sizeof(int));
+    printf("%d",sizeof(unsigned int));
     return 0;
 }
--- Firewall.h
+++ Firewall.h
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
+#define ACL_SIZE 99
+#define BLOCK_SIZE 10
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+typedef unsigned long long int int64;
 
 /*
 interface e0/0 --
@@ -10,62 +18,72 @@ security level 0 - e(Hexadecimal, 0 - 16 decimal)
 
 */
 
-typedef unsigned long long int uint64;
-typedef unsigned long int uint32;
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-
-typedef uint8 inter_num;
-typedef uint64 mac_address;
-typedef uint32 ip_address;
-typedef uint8 CIDR;
-typedef uint8 level; 
-typedef uint16 pincode;
-typedef uint8 subkey;//?
-typedef uint32 rounds; //?
-
-typedef enum e_action{
+enum e_action{
     permit = 1,
     drop = 2
-}action;
+};
+
+typedef enum e_action action;
 
 typedef struct s_network
 {
-    ip_address ip : 32; // 32 bits for IP address
-    CIDR subnet : 5; // 5 bits for CIDR (0-32)
+    unsigned int ip : 32; // 32 bits for IP address
+    unsigned char subnet : 5; // 5 bits for CIDR (0-32)
 }network;
 
 typedef struct s_user{
-    uint8 username[16]; // Username up to 31 characters + null terminator
-    pincode pin : 14; // 10 bits for pincode (0-10000)
+    unsigned char username[16]; // Username up to 31 characters + null terminator
+    unsigned int pin : 14; // 10 bits for pincode (0-10000)
     bool root : 1; // 1 bit for root status
 }user;
 
 typedef struct s_stdce{ // standard control entry structure
-    action act : 3 // 3 bits for action
+    unsigned char act : 3; // 3 bits for action
     network net; // network structure
 
 }stdace;
 
-typedef stdace[100] acl; // access control list with max 100 entries
+typedef stdace stdacl[ACL_SIZE]; // access control list with max 100 entries
 
 typedef struct s_rootkey{
-    subkey key : 10; // 10 bits for rootkey
-    rounds n : 23; // 23 bits for rounds (0-8000000)
+    unsigned int key : 10; // 10 bits for rootkey
+    unsigned long int n : 23; // 23 bits for rounds (0-8000000)
 }rootkey;
 
 typedef struct s_interface
 {
-    inter_num id : 3; //3 bits - 8 interfaces
-    mac_address mac : 48; //48 bits for mac_address
+    unsigned char id : 3; //3 bits - 8 interfaces
+    unsigned long long int mac : 48; //48 bits for mac_address
     network net; //network structure
-    strcut {
+    struct {
         bool l1 : 1;
         bool l3 : 1;
-    }shutdown : 2; //2 bits for shutdown status
-    uint8 zone_name[16]; // Zone name up to 15 characters + null terminator
-    level sec_level : 4; //4 bits for security level (0-16)
-    user account; //user structure
-    rootkey key; //rootkey structure ?
-    acl access_list; //access control list
+    };
+    unsigned char zone_name[16]; // Zone name up to 15 characters + null terminator
+    unsigned char sec_level : 4; //4 bits for security level (0-16)
+    stdacl *aclin; //Inbound ACL - ACL for incoming packets
+    stdacl *aclout; //Outbound ACL - ACL for outcoming packets
 }interface;
+
+typedef struct s_config{
+    interface *iface[7];
+    user account;
+    rootkey key;
+    stdacl *acl[ACL_SIZE];
+}config;
+
+
+typedef struct s_dynamic{
+    unsigned int count;// Number of values
+    unsigned int size; // Actual size of the dynamic array
+    unsigned int capacity; // The capacity the array can hold for now
+    unsigned int *data; // The data stored in the array - can be at any type
+}dynamic;
+
+dynamic* dynMake_(unsigned int);
+
+
+
+void dynAdd(dynamic* arr, void* val);
+
+
